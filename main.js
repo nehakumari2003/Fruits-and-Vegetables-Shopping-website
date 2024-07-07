a2a9cb3a28025b71694e9a6be3bf64d5f67dfcb5
nehakumari <yneha4295@gmail.com>
2024-07-07 20:21:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,4 +6,16 @@ var swiper = new Swiper(".home", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
